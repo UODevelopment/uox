aa12145083efc66ff8eb98ada15d7898dadb1919
cmkerr59@icloud.com <128108973+OldFoolAgain@users.noreply.github.com>
2023-10-26 11:52:02 -0400

    placeholder

---
+++ server.hpp
@@ -0,0 +1,13 @@
+//Copyright © 2023 Charles Kerr. All rights reserved.
+
+#ifndef server_hpp
+#define server_hpp
+#if defined(_WIN32) 
+#include <winsock2.h>
+#include <windows.h>
+#endif
+class Server {
+
+};
+
+#endif 
